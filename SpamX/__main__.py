56527b17b006382bbc22b63b556fb0b47bfda711
MrH4CK3R474 <148775562+MrH4CK3R474@users.noreply.github.com>
2023-12-11 12:29:02 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -3,7 +3,7 @@ from . import *
 
 if DATABASE_URL:
    from .database import users_db
-   print("SpamX - [INFO]: Adding all sudos in DB!")
+   print("💞𝙎𝙐𝘾𝘾𝙀𝙎𝙎𝙁𝙐𝙇 💦𝘿𝙀𝙋𝙇𝙊𝙔𝙀𝘿 𝘽𝙔 𝘾𝙊𝘿𝙀𝙓 🛑")
    for x in sudoser:
       users_db.addsudo(x)
      
