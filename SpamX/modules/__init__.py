d836e06946c5ac6be64c1b41e3eb74920f259a51
MrH4CK3R474 <148775562+MrH4CK3R474@users.noreply.github.com>
2023-12-11 13:41:34 +0530

    Update __init__.py

--- SpamX/modules/__init__.py
+++ SpamX/modules/__init__.py
@@ -9,7 +9,7 @@ __version__ = __version__
 
 
 ping_msg = PING_MSG if PING_MSG else "🇨Ⓞ𝗗𝜩🇽"
-pic = ALIVE_PIC if ALIVE_PIC else ""
+pic = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/0d7c524a3fe2798398b3a.jpg"
 amsg = ALIVE_MSG if ALIVE_MSG else "𝐎𝐏 ѕραм - by 𝗖ⓞ𝗗𝜩𝗫"
 
 try:
