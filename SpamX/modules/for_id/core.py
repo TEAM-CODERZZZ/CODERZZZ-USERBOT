6409c03e42fe85b8f6157b2181fb5b4abb9845a0
synax10 <150779327+synax10@users.noreply.github.com>
2023-12-11 18:54:35 +0530

    Update core.py

--- SpamX/modules/for_id/core.py
+++ SpamX/modules/for_id/core.py
@@ -1,6 +1,6 @@
 """
      SpamX - Telegram Bots
-     © CODEX - 2022-2023
+     © ⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️ - 2022-2023
 """
 import os, sys, asyncio, datetime, time, subprocess
 from .. import handler, Owner, Sudos, ping_msg, __version__
@@ -43,7 +43,7 @@ async def ping_me(_, e: Message):
 @Client.on_message(filters.user(Owner) & filters.command(["getvars", "getvar"], prefixes=handler))
 @Client.on_message(filters.me & filters.command(["getvars", "getvar"], prefixes=handler))
 async def all_vars(_, message: Message):
-    await message.reply_text(f"All Variables given below 👇\n\n {Variables_text} \n\n © @RiZoeLX")
+    await message.reply_text(f"All Variables given below 👇\n\n {Variables_text} \n\n © @ASHWANI10")
 
 @Client.on_message(filters.user(Owner) & filters.command(["scrape", "inviteall"], prefixes=handler))
 @Client.on_message(filters.me & filters.command(["scrape", "inviteall"], prefixes=handler))
@@ -70,7 +70,7 @@ async def scrape_members(SpamX: Client, message: Message):
            await asyncio.sleep(2)
         except Exception as a:
            print(f"[SpamX INFO]: {str(a)}")
-      return await Spamx.send_message(message.chat.id, f"**Users Added!** \nFrom chat: @{cht.username} \nTotal users added: `{added}` \n\n © @RiZoeLX")
+      return await Spamx.send_message(message.chat.id, f"**Users Added!** \nFrom chat: @{cht.username} \nTotal users added: `{added}` \n\n © @ASHWANI10")
    else:
       await message.reply_text(f"**Wrong usage** \n syntax: {handler}scrape @chatlink")
 
@@ -208,7 +208,7 @@ async def os_system(SpamX: Client, message: Message):
        if check_var in Variables:
           var = check_var
        else:
-          await message.reply_text(f"Wrong variable! All Variables given below 👇\n\n {Variables_text} \n\n © CODEX")
+          await message.reply_text(f"Wrong variable! All Variables given below 👇\n\n {Variables_text} \n\n © ⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️")
           return
        value = str(txt[1])
        try:
@@ -218,6 +218,6 @@ async def os_system(SpamX: Client, message: Message):
          os.execl(sys.executable, *args)
          quit()
        except Exception as error:
-         await message.reply_text(f"Error: {error} \n\n Report in CODEX")
+         await message.reply_text(f"Error: {error} \n\n Report in ⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️")
     else:
        await message.reply_text(f"**Wrong Usage** \n Syntax: {handler}setvar (var name) (value) \n\n Type `{handler}getvars` To get all Vars name!")
