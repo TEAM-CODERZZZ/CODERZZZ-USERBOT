6646a621cc2121c39b0fb66f2b2e715868a1ffa0
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-01-30 22:46:05 -0800

    Update config.py

--- SpamX/config.py
+++ SpamX/config.py
@@ -11,11 +11,11 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # -------------CONFIGS--------------------
-API_ID = int(os.getenv("API_ID", "20310034"))
+API_ID = int(os.getenv("API_ID", "24040183"))
 if not API_ID:
    print("SpamX [INFO]: You didn't fill API_ID var!")
    sys.exit()
-API_HASH = os.getenv("API_HASH", "e0d2c11f4ba291ce596868e73df87519")
+API_HASH = os.getenv("API_HASH", "d236dffc3dd60f89a149557898370e74")
 if not API_HASH:
    print("SpamX [INFO]: You didn't fill API_HASH var!")
    sys.exit()
@@ -60,7 +60,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", None))
 if not OWNER_ID:
    print("SpamX [INFO]: You didn't fill OWNER_ID var!")
    sys.exit()
-SUDO_USERS = os.getenv("SUDO_USERS", "6617592823")
+SUDO_USERS = os.getenv("SUDO_USERS", "6084527452")
 
 #Optional
 DATABASE_URL = os.getenv("DATABASE_URL", None)
